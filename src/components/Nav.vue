a3299d32cc0762cf8517e89a2a700e51167063bb
relara <relvira_7232@stu.southtexascollege.edu>
2022-03-25 19:54:20 -0500

    added detail movies

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,8 +1,9 @@
 <template>
 <div class="text-green-200 bg-green-900">
    <div class="container flex items-center justify-center mx-auto">
-    <h1 class="py-8 text-3xl font-thin tracking-tighter">Advice<span class="font-normal">Call</span>
-    </h1>
+    <router-link to="/">
+    <h1 class="py-8 text-3xl font-thin tracking-tighter">LOTR<span class="font-normal">Movies</span>
+    </h1></router-link>
     </div> 
 </div>
 
